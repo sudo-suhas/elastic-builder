b3190f1eb6a99e1516e9e6c258a8d3e5c3772697
Suhas Karanth <sudo.suhas@gmail.com>
2017-05-16 08:23:49 +0530

    fix(score-functions): rm invalid comment for WeightScoreFunction export

--- src/queries/compound-queries/score-functions/index.js
+++ src/queries/compound-queries/score-functions/index.js
@@ -2,7 +2,7 @@
 
 exports.ScoreFunction = require('./score-function');
 exports.ScriptScoreFunction = require('./script-score-function');
-exports.WeightScoreFunction = require('./weight-score-function'); // Alias for `ScoreFunction`
+exports.WeightScoreFunction = require('./weight-score-function');
 exports.RandomScoreFunction = require('./random-score-function');
 exports.FieldValueFactorFunction = require('./field-value-factor-function');
 exports.DecayScoreFunction = require('./decay-score-function');
