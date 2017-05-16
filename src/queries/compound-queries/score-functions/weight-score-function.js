0f77de5c9b39585ad822433469b2e2dcbd899ebc
Suhas Karanth <sudo.suhas@gmail.com>
2017-05-16 08:08:33 +0530

    fix(WeightScoreFunction): Fix invalid implementation

--- src/queries/compound-queries/score-functions/weight-score-function.js
+++ src/queries/compound-queries/score-functions/weight-score-function.js
@@ -1,6 +1,10 @@
 'use strict';
 
+const isNil = require('lodash.isnil');
+
 const ScoreFunction = require('./score-function');
+
+const { util: { recursiveToJSON } } = require('../../../core');
 
 /**
  * The `weight` score allows you to multiply the score by the provided `weight`.
@@ -11,10 +15,37 @@ const ScoreFunction = require('./score-function');
  * [Elasticsearch reference](https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-function-score-query.html#function-weight)
  *
  * @example
- * const scoreFunc = bob.weightScoreFunction().weight(42);
+ * const scoreFunc = bob.weightScoreFunction(42);
  *
- * @param {string} name
+ * @param {number=} weight The weight of this score function.
  */
-class WeightScoreFunction extends ScoreFunction {}
+class WeightScoreFunction extends ScoreFunction {
+    // eslint-disable-next-line require-jsdoc
+    constructor(weight) {
+        /*
+            null to `super` is intentional.
+            The following is a valid score function
+            It doesn't have a name field
+            {
+                "filter": { "match": { "test": "cat" } },
+                "weight": 42
+            }
+        */
+        super(null);
+
+        if (!isNil(weight)) this._body.weight = weight;
+    }
+
+    /**
+     * Overrides default `toJSON` to return DSL representation of the score function
+     * class instance.
+     *
+     * @override
+     * @returns {Object} returns an Object which maps to the elasticsearch query DSL
+     */
+    toJSON() {
+        return recursiveToJSON(this._body);
+    }
+}
 
 module.exports = WeightScoreFunction;
