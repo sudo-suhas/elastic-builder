653165fa19d0fbdcd3955723de69bd1848bab5b9
Suhas Karanth <sudo.suhas@gmail.com>
2018-01-01 08:50:38 +0530

    chore: Migrate to semantic-release@12

---
+++ deploy-docs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -xe
+# See http://tldp.org/LDP/abs/html/options.html
+# -x -> Print each command to stdout before executing it, expand commands
+# -e -> Abort script at first error, when a command exits with non-zero status
+#   (except in until or while loops, if-tests, list constructs)
+
+if ! hash gh-pages 2> /dev/null; then
+    yarn global add gh-pages
+fi
+
+gh-pages --add \
+    --dist . \
+    --src "{browser/*,docs/*}" \
+    --repo "https://$GH_TOKEN@github.com/sudo-suhas/elastic-builder.git" \
+    --message "docs: Build docs for `npm run -s print-version`"
