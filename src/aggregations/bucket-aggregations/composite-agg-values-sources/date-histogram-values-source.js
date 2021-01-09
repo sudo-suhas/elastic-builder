2f2ecfc2c266d1f3c8c1e2ea170935f7d501933c
Arnaud Cortisse <arnaud@koopol.com>
2021-01-09 16:12:05 +0100

    new: added support for DateHistogramValuesSource.calendarInterval && DateHistogramValuesSource.fixedInterval

--- src/aggregations/bucket-aggregations/composite-agg-values-sources/date-histogram-values-source.js
+++ src/aggregations/bucket-aggregations/composite-agg-values-sources/date-histogram-values-source.js
@@ -47,6 +47,50 @@ class DateHistogramValuesSource extends ValuesSourceBase {
     }
 
     /**
+     * Calendar-aware intervals are configured with the calendarInterval parameter.
+     * The combined interval field for date histograms is deprecated from ES 7.2.
+     *
+     * @example
+     * const agg = esb.dateHistogramValuesSource('by_month', 'date').calendarInterval(
+     *     'month'
+     * );
+     *
+     * @param {string} interval Interval to generate histogram over.
+     * You can specify calendar intervals using the unit name, such as month, or as
+     * a single unit quantity, such as 1M. For example, day and 1d are equivalent.
+     * Multiple quantities, such as 2d, are not supported.
+     * @returns {DateHistogramValuesSource} returns `this` so that calls can be chained
+     */
+    calendarInterval(interval) {
+        this._aggsDef.calendar_interval = interval;
+        return this;
+    }
+
+    /**
+     * Fixed intervals are configured with the fixedInterval parameter.
+     * The combined interval field for date histograms is deprecated from ES 7.2.
+     *
+     * @param {string} interval Interval to generate histogram over.
+     * Intervals are a fixed number of SI units and never deviate, regardless
+     * of where they fall on the calendar. However, it means fixed intervals
+     * cannot express other units such as months, since the duration of a
+     * month is not a fixed quantity.
+     *
+     * @example
+     * const agg = esb.dateHistogramValuesSource('by_minute', 'date').calendarInterval(
+     *     '60s'
+     * );
+     *
+     * The accepted units for fixed intervals are:
+     * millseconds (ms), seconds (s), minutes (m), hours (h) and days (d).
+     * @returns {DateHistogramValuesSource} returns `this` so that calls can be chained
+     */
+    fixedInterval(interval) {
+        this._aggsDef.fixed_interval = interval;
+        return this;
+    }
+
+    /**
      * Sets the date time zone
      *
      * Date-times are stored in Elasticsearch in UTC. By default, all bucketing
--- test/aggregations-test/composite-agg-values-sources-test/date-histogram-values-source.test.js
+++ test/aggregations-test/composite-agg-values-sources-test/date-histogram-values-source.test.js
@@ -29,7 +29,39 @@ test('constructor sets arguments', t => {
     t.deepEqual(value, expected);
 });
 
+test('calendar_interval_is_set', t => {
+    const value = getInstance('by_day', 'date')
+        .calendarInterval('month')
+        .toJSON();
+    const expected = {
+        my_val_src: {
+            date_histogram: {
+                field: 'date',
+                calendar_interval: 'month'
+            }
+        }
+    };
+    t.deepEqual(value, expected);
+});
+
+test('fixed_interval_is_set', t => {
+    const value = getInstance('by_day', 'date')
+        .fixedInterval('90s')
+        .toJSON();
+    const expected = {
+        my_val_src: {
+            date_histogram: {
+                field: 'date',
+                fixed_interval: '90s'
+            }
+        }
+    };
+    t.deepEqual(value, expected);
+});
+
 test(setsAggType, DateHistogramValuesSource, 'date_histogram');
 test(setsOption, 'interval', { param: 5 });
 test(setsOption, 'timeZone', { param: 'America/Los_Angeles' });
 test(setsOption, 'format', { param: 'yyyy-MM-dd' });
+test(setsOption, 'calendar_interval', { param: 'month' });
+test(setsOption, 'fixed_interval', { param: '90s' });
