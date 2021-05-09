a12f337707122c44676ac4ec0734c94e169d2e55
Arnaud Cortisse <arnaud@koopol.com>
2021-05-09 15:44:08 +0200

    feat: Add calendar_interval, fixed_interval to date-histogram values source (#133)
    Combined interval field was deprecated in Elasticsearch 7.2.

--- src/index.d.ts
+++ src/index.d.ts
@@ -4792,6 +4792,32 @@ declare namespace esb {
             interval(interval: string | number): this;
 
             /**
+             * Calendar-aware intervals are configured with the calendarInterval parameter.
+             * The combined interval field for date histograms is deprecated from ES 7.2.
+             *
+             * @param {string} interval Interval to generate histogram over.
+             * You can specify calendar intervals using the unit name, such as month, or as
+             * a single unit quantity, such as 1M. For example, day and 1d are equivalent.
+             * Multiple quantities, such as 2d, are not supported.
+             */
+            calendarInterval(interval: string): this;
+
+            /**
+             * Fixed intervals are configured with the fixedInterval parameter.
+             * The combined interval field for date histograms is deprecated from ES 7.2.
+             *
+             * @param {string} interval Interval to generate histogram over.
+             * Intervals are a fixed number of SI units and never deviate, regardless
+             * of where they fall on the calendar. However, it means fixed intervals
+             * cannot express other units such as months, since the duration of a
+             * month is not a fixed quantity.
+             *
+             * The accepted units for fixed intervals are:
+             * millseconds (ms), seconds (s), minutes (m), hours (h) and days (d).
+             */
+            fixedInterval(interval: string): this;
+
+            /**
              * Sets the date time zone
              *
              * Date-times are stored in Elasticsearch in UTC. By default, all bucketing
--- test/aggregations-test/composite-agg-values-sources-test/date-histogram-values-source.test.js
+++ test/aggregations-test/composite-agg-values-sources-test/date-histogram-values-source.test.js
@@ -29,7 +29,41 @@ test('constructor sets arguments', t => {
     t.deepEqual(value, expected);
 });
 
+test('calendar_interval_is_set', t => {
+    const value = getInstance('field_name', 'date')
+        .calendarInterval('month')
+        .toJSON();
+    const expected = {
+        my_val_src: {
+            date_histogram: {
+                field: 'field_name',
+                calendar_interval: 'month',
+                interval: 'date'
+            }
+        }
+    };
+    t.deepEqual(value, expected);
+});
+
+test('fixed_interval_is_set', t => {
+    const value = getInstance('field_name', 'date')
+        .fixedInterval('90s')
+        .toJSON();
+    const expected = {
+        my_val_src: {
+            date_histogram: {
+                field: 'field_name',
+                fixed_interval: '90s',
+                interval: 'date'
+            }
+        }
+    };
+    t.deepEqual(value, expected);
+});
+
 test(setsAggType, DateHistogramValuesSource, 'date_histogram');
 test(setsOption, 'interval', { param: 5 });
 test(setsOption, 'timeZone', { param: 'America/Los_Angeles' });
 test(setsOption, 'format', { param: 'yyyy-MM-dd' });
+test(setsOption, 'calendarInterval', { param: 'month' });
+test(setsOption, 'fixedInterval', { param: '90s' });
