36cf778a2dc82942695ee755a77ca0f450082191
Suhas Karanth <sudo.suhas@gmail.com>
2022-11-12 14:38:11 +0530

    fix: Specify user in deploy-docs for gh-pages

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -12,4 +12,5 @@ gh-pages --add \
     --dist . \
     --src "{browser/*,docs/*}" \
     --repo "https://$GITHUB_TOKEN@github.com/sudo-suhas/elastic-builder.git" \
-    --message "docs: Build docs for $(npm run -s print-version)"
+    --message "docs: Build docs for $(npm run -s print-version)" \
+    --user "github-actions-bot <support+actions@github.com>"
