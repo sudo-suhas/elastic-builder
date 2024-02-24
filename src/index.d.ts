dc4dbb651f89224d0732a40c68741a9fdef0d497
Alexander Reelsen <alexander@reelsen.net>
2024-02-24 15:27:47 +0100

    Update src/index.d.ts
    Co-authored-by: Suhas Karanth <sudo-suhas@users.noreply.github.com>

--- src/index.d.ts
+++ src/index.d.ts
@@ -3274,6 +3274,8 @@ declare namespace esb {
      *
      * [Elasticsearch reference](https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-rank-feature-query.html)
      *
+     * NOTE: This query was added in elasticsearch v7.0.
+     *
      * @example
      * const query = new RankFeatureQuery('rank_feature_field');
      *   query
